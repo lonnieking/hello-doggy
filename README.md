28f158bd574001afde1dcde22ab9cd506ba571e6
Lonnie King <lonnie.king@me.com>
2019-05-28 10:02:34 +1000

    created a hello doggy app

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hello Doggy
+
+### Hi, doggy!
+
+# How To Use
+```
+#install
+npm i
+
+#build
+npm build
+
+#run
+npm start
+```
+
+Go to `localhost:3000` in your browser of choice.
+
+You're my favorite customer.
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    <img src='hello_doggy.gif' alt="Hi, doggy."></img>,
+    document.getElementById('mount')
+  );
+});
